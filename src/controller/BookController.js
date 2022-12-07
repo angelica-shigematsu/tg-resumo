c8a7d6cac4ce72a25ced9d14dc523bff1a0fbc8e
Angélica <angelica_shigematsu@outlook.com>
2022-12-07 17:13:45 -0300

    added controller book and front-end

--- src/controller/BookController.js
+++ src/controller/BookController.js
@@ -1,7 +1,13 @@
 const Writer = require('../model/Writer')
 
-async function listVolunteers ( req, res){
-  await Writer.findAll({raw: true, order})
+async function getForeignKey(req, res){
+  await Writer.findAll({ raw : true, order: [
+    ['nameWriter', 'ASC']//ordem decrescente
+  ]}).then(writers => {
+    res.render("book", {
+        writers: writers
+    })
+  })
 }
 
-module.exports = { listVolunteers }
+module.exports = { getForeignKey }
