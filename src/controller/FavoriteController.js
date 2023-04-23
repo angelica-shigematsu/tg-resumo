e350e2cbe14c2aa63447c96c86c2bdffcc6964b2
Angélica <angelica_shigematsu@outlook.com>
2023-04-23 08:49:34 -0300

    add business rule to favorite summaries

--- src/controller/FavoriteController.js
+++ src/controller/FavoriteController.js
@@ -5,7 +5,11 @@ const { getSummary, getRating } = require('./RatingController');
 async function createFavorite(req, res) {
   const { refUser, refSummary } = req.body
 
-  created_at = new Date();
+  let profile = await getUserInformation(req, res)
+  let menu = await getlevelUser(profile)
+  let admin = await getlevelAdmin(profile)
+  let volunteer = await getlevelVolunteer(profile)
+  let created_at = new Date();
 
   try{
 
@@ -14,6 +18,29 @@ async function createFavorite(req, res) {
     const summary = await getSummary(refSummary);
 
     const ratings = await getRating(refSummary);
+
+    const hasAddOnFavorite = await Favorite.findOne({
+      where: { 
+        refUser: profile.id,
+        refSummary: refSummary
+      }
+    })
+    
+    if(hasAddOnFavorite) { 
+      return res.render('rating', { 
+        book: summary.book.title,   
+        ratings: ratings,
+        summary: summary,
+        volunteer: summary.user.fullName, 
+        writer: summary.writer.nameWriter, 
+        messageFavorite: 'Já foi adiconado como favorito',
+        refUserComment: refUserComment,
+        profile,
+        menu: menu, 
+        admin: admin,
+        volunteer: volunteer
+    })
+  }
 
     await Favorite.create({
       created_at,
@@ -28,7 +55,11 @@ async function createFavorite(req, res) {
       volunteer: summary.user.fullName, 
       writer: summary.writer.nameWriter, 
       messageFavorite: 'Adicionado como favorito com sucesso',
-      refUserComment: refUserComment
+      refUserComment: refUserComment,
+      profile,
+      menu: menu, 
+      admin: admin,
+      volunteer: volunteer
     })
   }catch(err) {
     res.json('erro')
@@ -44,5 +75,27 @@ async function getUserInformation(req, res) {
     return profile
   }
 }
+
+async function getlevelUser(profile) {
+  if (profile.level == 'Usuario')
+    return false
+  else
+    return true
+}
+
+async function getlevelVolunteer(profile) {
+  if (profile.level == 'Voluntario')
+    return true
+  else
+    return false
+}
+
+async function getlevelAdmin(profile) {
+  if (profile.level == 'Administador')
+    return true
+  else
+    return false
+}
+
 
 module.exports = { createFavorite }
