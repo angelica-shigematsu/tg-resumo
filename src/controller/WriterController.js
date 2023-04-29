cf1da314c295cca5f924b19e77defd2b7c5ceba8
Angélica <angelica_shigematsu@outlook.com>
2023-04-29 20:09:06 -0300

    alter writer

--- src/controller/WriterController.js
+++ src/controller/WriterController.js
@@ -1,26 +1,66 @@
 const Writer = require('../model/Writer')
+const User = require('../model/User')
+
+async function getInformationMenu(req, res) {
+  const profile = await getUserInformation(req, res)
+  let menu = await getlevelUser(profile);
+  let admin = await getlevelAdmin(profile)
+  let volunteer = await getlevelVolunteer(profile)
+
+  res.render("registerWriter", { 
+    message: false,  
+    messageError: false,
+    menu: menu,
+    admin: admin,
+    volunteer: volunteer,
+    profile: profile
+  })
+}
 
 async function createWriter(req, res) {
   const { nameWriter } = req.body
   const { dateBirthWriter } = req.body
 
   try{
+    const profile = await getUserInformation(req, res)
+    let menu = await getlevelUser(profile);
+    let admin = await getlevelAdmin(profile)
+    let volunteer = await getlevelVolunteer(profile)
+
     const writers = await showAllWriters()
+
     await Writer.create({
     nameWriter,
     dateBirthWriter
-  }).then(() => res.render("listAllWriters", { writers: writers, message: 'Criado com sucesso', messageError: false}))
+  }).then(() => res.render("listAllWriters", {
+     writers: writers, 
+     message: 'Criado com sucesso', 
+     messageError: false,
+     menu: menu,
+     admin: admin,
+     volunteer: volunteer,
+     profile: profile
+    }))
   }catch(error){
     res.json(error)
   }
 }
 
 async function listAllWriter(req, res) {
+  const profile = await getUserInformation(req, res)
+  let menu = await getlevelUser(profile);
+  let admin = await getlevelAdmin(profile)
+  let volunteer = await getlevelVolunteer(profile)
+
   const writers = await showAllWriters()
     res.render("listAllWriters", {
         writers: writers,
         message: false, 
-        messageError: false
+        messageError: false,
+        menu: menu,
+        admin: admin,
+        volunteer: volunteer,
+        profile: profile
   })
 } 
 
@@ -39,10 +79,21 @@ async function listWriter(req, res) {
 
   if(isNaN(numberId)) return res.redirect("/autor/listEscritor")
 
-    await Writer.findByPk(numberId).then(writers => {
+  const profile = await getUserInformation(req, res)
+  let menu = await getlevelUser(profile);
+  let admin = await getlevelAdmin(profile)
+  let volunteer = await getlevelVolunteer(profile)
+
+  await Writer.findByPk(numberId).then(writers => {
     if(!writers) return res.redirect("listEscritor")
 
-    res.render("listWriter", { writers: writers })
+    res.render("listWriter", { 
+      writers: writers,
+      menu: menu,
+      admin: admin,
+      volunteer: volunteer,
+      profile: profile 
+    })
   })
 } 
 
@@ -54,7 +105,13 @@ async function updateWriter(req, res) {
   if(!nameWriter && !dateBirthWriter )  res.render("listAllWriters")
 
   try{
+    const profile = await getUserInformation(req, res)
+    let menu = await getlevelUser(profile);
+    let admin = await getlevelAdmin(profile)
+    let volunteer = await getlevelVolunteer(profile)
+
     const writers = await showAllWriters()
+
     await Writer.update({
       nameWriter,
       dateBirthWriter
@@ -62,7 +119,15 @@ async function updateWriter(req, res) {
       where: {
         idWriter: id
     }}).then(() => {
-      res.render('listAllWriters', {  writers: writers, message: 'Alterado com sucesso', messageError: false})
+      res.render('listAllWriters', {  
+        writers: writers, 
+        message: 'Alterado com sucesso', 
+        messageError: false,
+        menu: menu,
+        admin: admin,
+        volunteer: volunteer,
+        profile: profile 
+      })
     })
   }catch(err) {
     res.status(400).send('Erro em atualizar os dados do Escritor')
@@ -72,19 +137,64 @@ async function updateWriter(req, res) {
 async function deleteWriter( req, res) {
   const { id } = req.body
 
+  const profile = await getUserInformation(req, res)
+  let menu = await getlevelUser(profile);
+  let admin = await getlevelAdmin(profile)
+  let volunteer = await getlevelVolunteer(profile)
+
   try{
     const writers = await showAllWriters()
     await Writer.destroy({
       where: { idWriter: id }
     }).then(() => {
-      return res.render("listAllWriters", {  writers: writers, message: 'Excluído com sucesso', messageError: false } )
+      return res.render("listAllWriters", { 
+        writers: writers, 
+        message: 'Excluído com sucesso', 
+        messageError: false,
+        menu: menu,
+        admin: admin,
+        volunteer: volunteer,
+        profile: profile 
+        } )
     })
   }catch(err) {
     res.status(400).send('Não pode excluir dados do escritor! Tem dependência de livro cadastrado!')
   }
 }
 
+async function getUserInformation(req, res) {
+  if (req.isAuthenticated()) {
+      const  { email } = req.user
+      const profile = await User.findOne({
+        where: { email: email}
+    })
+    return profile
+  }
+}
+
+async function getlevelUser(profile) {
+  if (profile.level == 'Usuario')
+    return false
+  else
+    return true
+}
+
+async function getlevelAdmin(profile) {
+  if (profile.level == 'Administrador')
+    return true
+  else
+    return false
+}
+
+async function getlevelVolunteer(profile) {
+  if (profile.level == 'Voluntario')
+    return true
+  else
+    return false
+}
+
 module.exports = { 
+  getInformationMenu,
   createWriter, 
   listAllWriter, 
   listWriter, 
