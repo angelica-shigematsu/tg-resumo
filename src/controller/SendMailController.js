dfbf7db5ccd5776db688b8e51fb4829e0702cdaf
Angélica <angelica_shigematsu@outlook.com>
2023-05-21 22:42:09 -0300

    feat send mail to reset password

---
+++ src/controller/SendMailController.js
@@ -0,0 +1,39 @@
+const sendGridMail = require('@sendgrid/mail')
+const User = require('../model/User')
+const emailOng = require('../../config/fromMail')
+
+async function sendMail(req, res) {
+  const { email } = req.body
+  const API_KEY = ''
+
+  sendGridMail.setApiKey(API_KEY)
+
+  if (noExitsMail(email)) return res.render("index", { messageError: "Não existe esse email cadastrado"})
+
+  const message = {
+    to: email,
+    from: emailOng.email,
+    subject:'ColabBook - Redefinição de senha',
+    text: 'Segue o link de redefinição de senha: '
+  }
+
+  sendGridMail
+    .send(message)
+    .then((res) => {return res.render("index", { messageError: "Redefinido a senha com sucesso. Verifique seu email!"})})
+    .catch((error) => console.log("Error: " + error))
+}
+
+async function noExitsMail(email) {
+ 
+  const profile = await User.findOne({
+    where: { email: email}
+  })
+  
+  if (!profile) return true
+
+  return false
+}
+
+module.exports = {
+  sendMail
+}
